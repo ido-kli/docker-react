9c0a82255f1269978ef43d9977cd8ce8d6e39103
ido.klimovsky <ido.klimovsky@condat.de>
2022-09-11 10:57:49 +0200

    changed failed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/I was changed on the feature branch/i);
   expect(linkElement).toBeInTheDocument();
 });
